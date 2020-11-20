611a0ca2ad4e573c1f0c549f246f635366339860
luan-alencar <luanalencar134@gmail.com>
2020-11-20 11:11:17 -0300

    adicionando mapeamento persistentes nos atributos da Clsse Compra

--- src/main/java/david/augusto/luan/entidades/Compra.java
+++ src/main/java/david/augusto/luan/entidades/Compra.java
@@ -1,16 +1,39 @@
 package david.augusto.luan.entidades;
 
+import java.io.Serializable;
 import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@Table(name = "compra")
 @Data
 @Getter @Setter
-public class Compra {
+public class Compra implements Serializable{
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "valor")
 	private Long id;
+	
+	@Column(name = "valor")
 	private double valor;
+	@Column(name = "data")
 	private Date data;
+	@Column(name = "observacao")
 	private String observacao;
+	@Column(name = "recebido")
+	private boolean recebido;
+	
 }
