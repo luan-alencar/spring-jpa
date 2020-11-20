e3e2c2cdad43c6b4e626f54a6deca8b4ddc2ff9a
luan-alencar <luanalencar134@gmail.com>
2020-11-20 11:23:36 -0300

    adicionando construtor vazio e instruções

--- src/main/java/david/augusto/luan/entidades/EmProvider.java
+++ src/main/java/david/augusto/luan/entidades/EmProvider.java
@@ -2,13 +2,20 @@ package david.augusto.luan.entidades;
 
 import javax.persistence.EntityManagerFactory;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor
 public class EmProvider {
 	private static final String DB_PU = "AplicaçãoPU";
-	
+
 	public static final boolean DEBUG = true;
-	
+
 	public static final EmProvider singleton = new EmProvider();
-	
+
 	private EntityManagerFactory emf;
-	
+
+	public static EmProvider getInstance() {
+		return singleton;
+	}
 }
