fb796122b4c413ff9352c8b7893b1031601eaeb5
luan-alencar <luanalencar134@gmail.com>
2020-11-20 11:16:14 -0300

    mapeamento da classe Pessoa

--- src/main/java/david/augusto/luan/entidades/Pessoa.java
+++ src/main/java/david/augusto/luan/entidades/Pessoa.java
@@ -1,16 +1,34 @@
 package david.augusto.luan.entidades;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@Table(name = "pessoa")
 @Data
 @Getter @Setter
 public class Pessoa {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "id")
 	private Long id;
+	@Column(name = "loggin")
 	private String loggin;
+	@Column(name = "senha")
 	private String senha;
+	@Column(name = "email")
 	private String email;
+	@Column(name = "nome")
 	private String nome;
 }
